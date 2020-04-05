bd7ffa38d2fe8b6491987359c5bcd520938cd78b
Julein "djakk <djakk.djakk@gmail.com>
2020-04-05 11:13:11 +0200

    bitmap map -> vector map

--- html/index.js
+++ html/index.js
@@ -1,9 +1,19 @@
-var map = L.map('the_map').setView([51.505, -0.09], 13);
+var map = L.map('the_map').setView([48.11836, -1.68727], 18); // Anatole France street, Rennes, Brittany
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+/*L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
 L.marker([51.5, -0.09]).addTo(map)
     .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-    .openPopup();
+    .openPopup();*/
+
+
+var mapboxUrl = "https://https://openstreetmap-on-heroku.herokuapp.com/{z}/{x}/{y}.mvt";
+var mapboxVectorTileOptions = {
+			rendererFactory: L.canvas.tile,
+			attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://www.mapbox.com/about/maps/">MapBox</a>',
+			vectorTileLayerStyles: vectorTileStyling
+};
+var mapboxPbfLayer = L.vectorGrid.protobuf(mapboxUrl, mapboxVectorTileOptions);
+mapboxPbfLayer.addTo(map);
