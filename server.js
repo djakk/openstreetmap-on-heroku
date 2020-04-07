50d8caba7977830cad3d526fc937c0f913890284
Julein "djakk <djakk.djakk@gmail.com>
2020-04-07 15:12:06 +0200

    mapnik style on server side is useless

--- server.js
+++ server.js
@@ -33,7 +33,8 @@ function get_an_integer_from_a_string(the_string) {
 
 // many thanks to https://smallmultiples.com.au/articles/building-large-maps-with-a-node.js-tile-server/ !!
 const createVectorTile = (sql,{ x, y, z }) => {
-  const map = new mapnik.Map(256, 256, proj4);  
+  const map = new mapnik.Map(256, 256, proj4);
+  // Actually, mapnik style on server side is useless except the Layer part
   var s = '<Map srs="+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext +no_defs">';
   s += '<Style name="points">';
   s += ' <Rule>';
